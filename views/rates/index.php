5a183f19ecfa5e4489c9be2f39a9884b990c0247
Ростислав <prozrostl@gail.com>
2016-11-03 16:09:52 +0300

    added style

--- views/rates/index.php
+++ views/rates/index.php
@@ -37,30 +37,54 @@ $this->title = 'Курс валют';
             <p><?php print_r($exchangeData["blackSale"])?></p>
         </div>
         <div class="avg_rate">
-            <h3>Средний курс на сегодня:</h3>
-            <?php print_r($avgSum)?>
+
+            <p><?= $avgSum ?>
         </div>
     </div>
-    
+
 
 
 
 <style>
+body{
+    background-color: #3333cc;
+}
 .links {
     display: block;
     margin-left: 80%;
+    color:white;
 }
 div {
     display: inline-block !important;
     overflow: auto;
     margin-left: 50px;
     vertical-align: top;
+    color:white;
 }
+
 .avg_rate {
     margin-left: 250px;
     margin-top: 6%;
 }
-a {
+.avg_rate p{
+    display: inline-block;
+    font-size: 45px;
+    margin-left: 75px;
+    color: #ffff00;
+}
+.links a {
     margin-left: 15px;
+    color:white;
+    border:1px solid white;
+    border-radius: 10px;
+    padding: 1px;
+    text-decoration: none;
+}
+.links a:active{
+    background-color: white;
+}
+.links a:hover{
+    background-color: white;
+    color:#3333cc;
 }
 </style>
